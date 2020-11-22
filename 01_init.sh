8b79e0e45c5281f68ea3e2b33b6aa15057e24ee3
Jeffrey Perkel <j_perkel@yahoo.com>
2020-11-21 18:52:48 -0700

    Remove some 'press any key' prompts

--- 01_init.sh
+++ 01_init.sh
@@ -23,46 +23,49 @@ read -p "Press enter to continue: " key
 
 echo -e "\nCreating working directory $NATUREDIR..."
 mkdir $NATUREDIR
-echo -e "Entering $NATUREDIR..."
+# echo -e "Entering $NATUREDIR..."
 cd $NATUREDIR
 
-echo -e "\nCreating dummy data files..."
-read -p "Press enter to continue: " key 
+echo -e "Creating dummy data files..."
+# read -p "Press enter to continue: " key 
 # create a set of files with nonstandard date stamps
 for month in "${months[@]}"; do
     for day in "${days[@]}"; do 
         fname=datafile-$day-$month-2020.txt
         # the 'touch' command creates an empty file
         touch $fname
-        echo "File $fname created."
+        # echo "File $fname created."
     done
 done
 
-echo -e "\nCreating dummy gene expression data..."
-read -p "Press enter to continue: " key 
+echo -e "Creating dummy gene expression data..."
+# read -p "Press enter to continue: " key 
 # create a CSV with two columns, 'gene' and 'count'...
 echo "gene","count" > $GENEFILE
 # add 1000 mock gene readings
 for i in {1..1000}; do
     echo GENE_$(($RANDOM % 20 + 1)),$(($RANDOM * 10)) >> $GENEFILE
 done 
-echo -e "File $GENEFILE created."
+# echo -e "File $GENEFILE created."
 
-echo -e "\nCreating background readings..."
-read -p "Press enter to continue: " key 
+echo -e "Creating background readings..."
+# read -p "Press enter to continue: " key 
 echo "sample","bkgd" > $BKGFILE
 for i in {1..50}; do            
     echo $i,$(($RANDOM / 100)) >> $BKGFILE
 done
-echo -e "File $BKGFILE created."
+# echo -e "File $BKGFILE created."
 
-echo -e "\nCreating data readings..."
-read -p "Press enter to continue: " key 
+echo -e "Creating data readings..."
+# read -p "Press enter to continue: " key 
 for i in 01 02 03; do
     fname=reading$i.csv
     echo "sample","abs$i" > $fname 
     for j in {1..50}; do
         echo $j,$RANDOM >> $fname
     done
-    echo "File $fname created."
+    # echo "File $fname created."
 done
+
+read -p "Press enter to view the file listing: " key 
+ls
